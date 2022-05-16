16e18480acbc47e9b684923a2af2390e309cbd28
Ahmed El-Helw <ahmedre@gmail.com>
2022-05-16 15:29:41 +0400

    Credentials now need to be passed in manually
    Support either a base64 encoded string parameter, or a file parameter.

--- src/main/kotlin/net/cafesalam/profileuploader/Main.kt
+++ src/main/kotlin/net/cafesalam/profileuploader/Main.kt
@@ -3,6 +3,9 @@ package net.cafesalam.profileuploader
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
+import com.github.ajalt.clikt.parameters.groups.required
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
@@ -11,15 +14,21 @@ import com.github.ajalt.clikt.parameters.types.int
 import net.cafesalam.profileuploader.sheets.SpreadsheetService
 import net.cafesalam.profileuploader.task.CheckDeltaUtil
 import net.cafesalam.profileuploader.task.GradleProfilerDataUploader
+import okio.ByteString.Companion.decodeBase64
 
 class BenchmarkUtil : NoOpCliktCommand()
 
 class CheckDeltas : CliktCommand() {
   private val width by option(help = "number of results to consider before and after each commit").int().default(5)
   private val threshold by option(help = "threshold that marks a build a delta in build times").int().default(25)
+  private val authenticationData: String by mutuallyExclusiveOptions(
+    option("--auth-file").file().convert { it.readText() },
+    option("--auth-string").convert { it.decodeBase64()?.utf8() ?: "" }
+  ).required()
 
   override fun run() {
-    val sheetsService = SpreadsheetService().getSheetsClient()
+    val spreadsheetService = SpreadsheetService()
+    val sheetsService = spreadsheetService.getSheetsClient(authenticationData)
     CheckDeltaUtil.checkForNotableDelta(sheetsService, width, threshold)
   }
 }
@@ -27,9 +36,14 @@ class CheckDeltas : CliktCommand() {
 class UploadProfilingData : CliktCommand() {
   private val gitHash by option(help = "the git hash of this commit").required()
   private val benchmarkFile by option(help = "the gradle-profiler result csv file").file(mustExist = true).required()
+  private val authenticationData: String by mutuallyExclusiveOptions(
+    option("--auth-file").file().convert { it.readText() },
+    option("--auth-string").convert { it.decodeBase64()?.utf8() ?: "" }
+  ).required()
 
   override fun run() {
-    val sheetsService = SpreadsheetService().getSheetsClient()
+    val spreadsheetService = SpreadsheetService()
+    val sheetsService = spreadsheetService.getSheetsClient(authenticationData)
     GradleProfilerDataUploader.parseAndWriteBenchmarks(sheetsService, benchmarkFile, gitHash)
   }
 }
--- src/main/kotlin/net/cafesalam/profileuploader/sheets/SpreadsheetService.kt
+++ src/main/kotlin/net/cafesalam/profileuploader/sheets/SpreadsheetService.kt
@@ -6,22 +6,25 @@ import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.SheetsScopes
 import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
-import java.io.FileNotFoundException
+import java.io.ByteArrayInputStream
 
 class SpreadsheetService {
 
-  private fun getCredentialsFromServiceAccount(): GoogleCredentials {
-    val inputStream = SpreadsheetService::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-      ?: throw FileNotFoundException("could not find: $CREDENTIALS_FILE_PATH")
+  private fun getCredentialsFromServiceAccount(authenticationData: String): GoogleCredentials {
+    val inputStream = ByteArrayInputStream(authenticationData.toByteArray())
     val credentials = GoogleCredentials.fromStream(inputStream).createScoped(SCOPES)
     credentials.refreshIfExpired()
     return credentials
   }
 
-  fun getSheetsClient(): Sheets {
+  fun getSheetsClient(authenticationData: String): Sheets {
     val jsonFactory = GsonFactory.getDefaultInstance()
     val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
-    return Sheets.Builder(httpTransport, jsonFactory, HttpCredentialsAdapter(getCredentialsFromServiceAccount()))
+    return Sheets.Builder(
+      httpTransport,
+      jsonFactory,
+      HttpCredentialsAdapter(getCredentialsFromServiceAccount(authenticationData))
+    )
       .setApplicationName(APPLICATION_NAME)
       .build()
   }
@@ -32,7 +35,6 @@ class SpreadsheetService {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private val SCOPES = listOf(SheetsScopes.SPREADSHEETS)
-    private const val CREDENTIALS_FILE_PATH = "/service_account.json"
     private const val APPLICATION_NAME = "Gradle Profiler Info Uploader"
   }
 }
