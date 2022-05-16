e4440cc5503b945ca8744ddf493767f8250b0f51
Ahmed El-Helw <ahmedre@gmail.com>
2022-05-16 18:17:42 +0400

    Add an r8Jar for shrinking the jar file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+configurations {
+  create("r8")
+}
+
 plugins {
   application
   kotlin("jvm") version "1.6.21"
@@ -14,9 +18,11 @@ application {
 
 repositories {
   mavenCentral()
+  google()
 }
 
 dependencies {
+  "r8"("com.android.tools:r8:3.3.28")
   implementation("com.squareup.okio:okio:3.1.0")
   implementation("com.github.ajalt.clikt:clikt:3.4.2")
 
@@ -30,14 +36,44 @@ dependencies {
 }
 
 val fatJar = task("fatJar", type = Jar::class) {
-  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-  from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-  with(tasks.jar.get() as CopySpec)
+  dependsOn(project.configurations["runtimeClasspath"])
+  dependsOn(tasks.named("jar"))
 
   archiveClassifier.set("fat")
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
   manifest {
     attributes["Main-Class"] = "net.cafesalam.profileuploader.MainKt"
+  }
+
+  val sourceClasses = sourceSets.main.get().output.classesDirs
+  inputs.files(sourceClasses)
+
+  doFirst {
+    from(sourceClasses)
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+  }
+}
+
+val r8File = File("$buildDir/libs/profileuploader-r8.jar")
+val r8Jar = task("r8Jar", type = JavaExec::class) {
+  dependsOn(project.configurations["runtimeClasspath"])
+  inputs.files(fatJar.outputs.files)
+  outputs.file(r8File)
+
+  classpath = project.configurations["r8"]
+  mainClass.set("com.android.tools.r8.R8")
+  val arguments = mutableListOf(
+    "--release",
+    "--classfile",
+    "--output", r8File.toString(),
+    "--pg-conf", "src/main/rules.txt",
+    "--lib", System.getProperty("java.home").toString()
+  )
+
+  doFirst {
+    arguments.add(fatJar.archiveFile.get().asFile.absolutePath)
+    args = arguments
   }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,2 @@
-
-rootProject.name = "profiler-uploader"
+rootProject.name = "profile-uploader"
 
