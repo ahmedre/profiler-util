6214edd8dddb33050518e7e52bf0ad071512a5b6
Ahmed El-Helw <ahmedre@gmail.com>
2022-05-23 17:53:23 +0400

    Fix bugs in result checker

--- src/main/kotlin/net/cafesalam/profilerutil/benchmark/BenchmarkChecker.kt
+++ src/main/kotlin/net/cafesalam/profilerutil/benchmark/BenchmarkChecker.kt
@@ -8,12 +8,12 @@ object BenchmarkChecker {
 
   fun checkResults(before: List<List<Any>>, after: List<List<Any>>, threshold: Int): List<BenchmarkStep> {
     // each row has <date> <git hash> [scenario_1_time, scenario_2_time ... scenario_n_time
-    val runs = min(before.minOf { it.size }, after.minOf { it.size })
+    val runs = min(before.minOf { it.size }, after.minOf { it.size }) - 2
     return (1..runs).map { scenarioNumber ->
       // index 0 is the date, index 1 is the hash, so scenario 1 is index 2
       val scenarioIndex = scenarioNumber + 1
-      val runsBefore = before.map { it[scenarioIndex] as Double }
-      val runsAfter = after.map { it[scenarioIndex] as Double }
+      val runsBefore = before.map { it[scenarioIndex].toString().toDouble() }
+      val runsAfter = after.map { it[scenarioIndex].toString().toDouble() }
 
       val stepDelta = StepFit.stepFit(runsBefore, runsAfter)
       if (stepDelta.absoluteValue >= threshold) {
